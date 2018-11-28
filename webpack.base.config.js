d7f22a5b597c63ea7497122462c64b4bfe906dd6
Uche Nnadi <unnadi@pixsystem.com>
2018-11-28 14:36:57 -0800

    [add] minor nit clean up

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -1,7 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
 const ImageminPlugin = require("imagemin-webpack");
 const imageminGifsicle = require("imagemin-gifsicle");
 const imageminJpegtran = require("imagemin-jpegtran");
