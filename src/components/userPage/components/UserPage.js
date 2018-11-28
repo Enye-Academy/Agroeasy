820af799524e5c8ecc12f52b8fbd1e502c6cd14e
Joshua Obinna <johuaobinna93@gmail.com>
2018-11-28 05:06:36 +0100

    building

---
+++ src/components/userPage/components/UserPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Col, Row, Layout } from 'antd';
+
+const { Content, Heading, sider } = Layout;
+
+//this is the userpage component
+class UserPage extends React.Component {
+    render() {
+        return(
+
+        );
+    }
+}
+
+export default UserPage;
