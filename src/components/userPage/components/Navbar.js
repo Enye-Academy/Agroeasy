61e7cd07468d7fd054a10958eb93ea03f4dc6d85
Joshua Obinna <johuaobinna93@gmail.com>
2018-11-29 05:48:21 +0100

    staged

--- src/components/userPage/components/Navbar.js
+++ src/components/userPage/components/Navbar.js
@@ -2,32 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
 
-import ContactUs from './ContactUs';
-import { GOODS, HOME, LOGO, NAVIBAR, SUPPORT } from './constants';
+import { NAVBAR } from './constants';
 
-const { GOODS_TEXT, HOME_TEXT, FLOAT_NAV, NAV_LINK, NAV_MODE, NAV_THEME, SUPPORT_TEXT } = NAVIBAR;
-const { SOURCE, WIDTH } = LOGO;
+const { LINKS: { HOME } } = NAVBAY;
 const { Header } = Layout;
 
-/*
- * this is the the navigation bar at the top of the home page
- */
-
-export default class Navibar extends React.Component {
+export default class Navbar extends React.Component {
     render() {
         return (
             <Layout>
                 <Header>
                     <div><Link to={HOME}><img src={SOURCE} width={WIDTH} /></Link>
-                        <Menu theme={NAV_THEME} mode={NAV_MODE} className={FLOAT_NAV} >
+                        <Menu theme="dark" mode="horizontal" style={{ float: "right" }} >
                             <Menu.Item>
-                                <Link to={HOME} className={NAV_LINK}>{HOME_TEXT}</Link>
+                                <Link to={HOME}>{HOME_TEXT}</Link>
+                            </Menu.Item>
+                            <Menu.Item>
+                                <Link>Categorieses</Link>
                             </Menu.Item>
                             <Menu.Item>
                                 <Link to={SUPPORT} className={NAV_LINK}>{SUPPORT_TEXT}</Link>
-                            </Menu.Item>
-                            <Menu.Item>
-                                <Link to={GOODS} className={NAV_LINK}>{GOODS_TEXT}</Link>
                             </Menu.Item>
                             <Menu.Item className={NAV_LINK}><ContactUs /></Menu.Item>
                         </Menu>
