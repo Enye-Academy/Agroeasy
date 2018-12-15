a31a8c30178b769b5292d09db3838c5da5c584a8
Joshua Obinna <johuaobinna93@gmail.com>
2018-12-15 12:41:14 +0100

    UserProfile

--- src/components/userPage/components/UserInfo.js
+++ src/components/userPage/components/UserInfo.js
@@ -1,63 +1,35 @@
 import React from 'react';
 import { Avatar, Col, Divider, Layout, List, Row } from 'antd';
 
-import { FAKE_USER } from './constants';
+import { CONTACT, DATA, LOCATION, USER_PAGE } from './constants';
 
 const { Content } = Layout;
-const { address, city, country, email, firstName, gender, lastName, phoneNumber } = FAKE_USER;
+const { CLASSNAMES: { BASIC_INFO, CONTACT_INFO, LOCATION_INFO, PROFILE_INFO, INFO_ROW, INFO_LAYOUT,
+    USER_INFO_CONTENT },
+STRINGS: { CENTER, FLEX, ITEM_LAYOUT, ORIENTATION },
+TEXTS: { BASIC_INFO_TEXT, CONTACT_INFO_TEXT, LOCATION_INFO_TEXT } } = USER_PAGE;
 
 export default class UserInfo extends React.Component {
     render() { 
-        const data = [
-            {
-                BasicInfo: 'Firstname',  description: firstName,
-            },
-            {
-                BasicInfo: 'Lastname',  description: lastName,
-            },
-            {
-                BasicInfo: 'Gender',   description: gender,
-            },
-        ];
-        const contact = [
-            {
-                contact: "Address", description: address,
-            },
-            {
-                contact: "Email",   description: email,
-            },
-            {
-                contact: "PnoneNumber", description: phoneNumber,
-            },
-        ];
-
-        const location = [
-            {
-                description: "Lagos", title: city,
-            },
-            {
-                description: "Nigeria", title: country,
-            },
-        ];
         return(
-            <Layout className="user-info-layout">
-                <Content style={{ margin: '24px 16px 0' }}>
-                    <div className="avatar" style={{  }}>
+            <Layout className={INFO_LAYOUT}>
+                <Content className={USER_INFO_CONTENT}>
+                    <div className="avatar">
                         <Avatar
                             size={150} shape="circle"
                             src="./images/josh.jpg"
                         />
                     </div>
-                    <Row className="user-info-row" type="flex" justify="center">
-                        <Col span={6} style={{ marginRight: "100px" }}>
-                            <Divider orientation="left">Basic Info</Divider>   
-                            <List divider="disabled"
-                                itemLayout="horizontal"
-                                dataSource={data}
+                    <Row className={INFO_ROW} type={FLEX} justify={CENTER}>
+                        <Col span={6} className={BASIC_INFO}>
+                            <Divider orientation={ORIENTATION}>{BASIC_INFO_TEXT}</Divider>   
+                            <List
+                                itemLayout={ITEM_LAYOUT}
+                                dataSource={DATA}
                                 renderItem={item => (
                                     <List.Item>
                                         <List.Item.Meta
-                                            className="profile-info"
+                                            className={PROFILE_INFO}
                                             title={item.BasicInfo}
                                             description={item.description}
                                         /> 
@@ -65,15 +37,15 @@ export default class UserInfo extends React.Component {
                                 )}
                             />
                         </Col>
-                        <Col span={6} style={{ marginRight: "10px" }}>
-                            <Divider orientation="left">Contact Info</Divider>
+                        <Col span={6} className={CONTACT_INFO}>
+                            <Divider orientation={ORIENTATION}>{CONTACT_INFO_TEXT}</Divider>
                             <List 
-                                itemLayout="horizontal"
-                                dataSource={contact}
+                                itemLayout={ITEM_LAYOUT}
+                                dataSource={CONTACT}
                                 renderItem={item => (
                                     <List.Item>
                                         <List.Item.Meta
-                                            className="profile-info"
+                                            className={PROFILE_INFO}
                                             title={item.contact}
                                             description={item.description}
                                         />
@@ -81,15 +53,15 @@ export default class UserInfo extends React.Component {
                                 )}
                             />
                         </Col>
-                        <Col span={6} style={{ marginLeft: '60px' }}>
-                            <Divider orientation="left">Location</Divider>
+                        <Col span={6} className={LOCATION_INFO}>
+                            <Divider orientation={ORIENTATION}>{LOCATION_INFO_TEXT}</Divider>
                             <List 
-                                itemLayout="horizontal"
-                                dataSource={location}
+                                itemLayout={ITEM_LAYOUT}
+                                dataSource={LOCATION}
                                 renderItem={item => (
                                     <List.Item>
                                         <List.Item.Meta
-                                            className="profile-info"
+                                            className={PROFILE_INFO}
                                             title={item.title}
                                             description={item.description}
                                         />
--- src/components/userPage/components/constants.js
+++ src/components/userPage/components/constants.js
@@ -20,6 +20,7 @@ export const LOGO = {
     WIDTH: "60px",
 };
 
+/** 
 //Producer profile
 export const FAKE_USER = {
     address: '24, Wilkie Crescent, Ikeja',
@@ -31,13 +32,63 @@ export const FAKE_USER = {
     lastName: 'Obinna',
     phoneNumber: '+2347061589692',
 };
+*/
+
 //userpage string
 export const USER_PAGE = {
+    CLASSNAMES: {
+        BASIC_INFO: 'basic-info',
+        CONTACT_INFO: 'contact-info',
+        INFO_LAYOUT: 'info-layout', 
+        INFO_ROW: 'info-row',
+        LOCATION_INFO: 'location-info',
+        PROFILE_INFO: 'profile-info',
+        USER_INFO_CONTENT: 'user-info-content',
+    },
     STRINGS: {
+        CENTER: 'center',
+        FLEX: 'flex',
+        ITEM_LAYOUT: 'horizontal',
+        ORIENTATION: 'left',
         SM_HEADER: 'sm-header',
         SM_HEADER_DIV: 'sm-header-div',
     },
     TEXTS: {
+        BASIC_INFO_TEXT: 'Basic Info',
+        CONTACT_INFO_TEXT: 'Contact Info',
+        LOCATION_INFO_TEXT: 'Location',
         TITLE_TEXT: 'My Agroeasy',
     },
 };
+
+export const DATA = [
+    {
+        BasicInfo: 'Firstname',  description: 'Josh',
+    },
+    {
+        BasicInfo: 'Lastname',  description: 'Obinna',
+    },
+    {
+        BasicInfo: 'Gender',   description: 'male',
+    },
+];
+export const CONTACT = [
+    {
+        contact: "Address", description:  '24, Wilkie Crescent, Ikeja',
+    },
+    {
+        contact: "Email",   description: 'joshuaobinna@gmail.com',
+    },
+    {
+        contact: "PnoneNumber", description: +2347061589692,
+    },
+];
+
+export const LOCATION = [
+    {
+        description: "Lagos", title: 'city',
+    },
+    {
+        description: "Nigeria", title: 'country',
+    },
+];
