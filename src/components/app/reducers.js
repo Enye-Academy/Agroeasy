fff71447b578d21fab65ff1041c4464f67a3e80a
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-01-20 22:27:43 +0100

    get cookie

--- src/components/app/reducers.js
+++ src/components/app/reducers.js
@@ -7,7 +7,7 @@ const initialState = {
 
 export default ( state = { ...initialState }, action) => {
     switch (action.type) {
-        case SET_COOKIE:
+        case SET_COOKIE:    {
             const expiration = new Date().getTime()
             const token = action.data.token || 'testtoken';
             browser.cookies.set(token, expiration);
@@ -18,8 +18,15 @@ export default ( state = { ...initialState }, action) => {
                 user: { ...action.data.user }
 
             };
-        default:
-
+        }
+        case REMOVE_COOKIE: {
+            const token = action.data.token || 'testtoken';
+            browser.cookies.remove(token);
+            return {
+                ...state,
+                loggedIn: false,
+            }
+        }
     }
 }
  
