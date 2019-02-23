677e6369864745cd3aae5e84cf05ef7cf3dbf35b
Uche Nnadi <unnadi@pixsystem.com>
2019-02-22 16:32:04 -0800

    [remove] unneeded types from schema

--- db/models/product.js
+++ db/models/product.js
@@ -3,13 +3,10 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const ProductsSchema = new Schema({
-    createdAt: { type: Date },
-    deletedAt: { type: Date },
     name: { required: true, type: String },
     producerId: { required: true, type: String },
     quantity: { required: true, type: Number },
     typeOfProducts: { required: true, type: String },
-    updatedAt: { type: Date },
 }, { versionKey: false });
 
 export default mongoose.model('Product', ProductsSchema);
