fe0310eded618f895f8ae65f46bd0bfe8a28a5dd
Joshua Obinna <joshuaobinna93@gmail.com>
2019-03-11 11:57:27 +0100

    code clean up

---
+++ src/components/userProfile/actions.js
@@ -0,0 +1,6 @@
+import { FETCH_USER_DATA } from './actionTypes';
+
+export const fetchUserData = data => ({
+    data,
+    type: FETCH_USER_DATA,
+});
---
+++ src/components/userProfile/sagas.js
@@ -0,0 +1,40 @@
+/* import { effects } from 'redux-saga';
+import {  FETCH_USER_DATA } from './actionTypes';
+import { USER_DATA_URL } from './constants';
+import { fetchUserData, fetchUserFailure } from './actions';
+
+function* getUserData(action){
+    try {
+        const { data } = action;
+        /*         const response = yield fetch(USER_DATA_URL, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            method: 'GET',
+        });
+        if(response.ok){ 
+            const data = yield response.json();
+            console.log(data);
+        yield effects.put(fetchUserData(data));
+        
+    } catch(error){
+        yield effects.put(fetchUserFailure(error));
+        console.log(error);
+    }
+}
+
+function* watchfetchUserData(){
+    try {
+        yield effects.takeLatest(FETCH_USER_DATA, getUserData);
+    } catch(error){
+        // eslint-disable-next-line no-console
+        console.log(error);
+    }
+}
+
+export default function* (){
+    yield effects.all([
+        watchfetchUserData(),
+    ]);
+}
+ */
